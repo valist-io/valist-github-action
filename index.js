e2969a5ccd58e9e7dd93030867b18d10b025820d
Alec M. Wantoch <alec@wantoch.com>
2021-08-27 00:12:29 -0400

    fix console logs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 
 const cmd = spawn(`bash -c "${path.join(__dirname, '/bin/valist')} publish --dryrun"`, { shell: true });
 
-cmd.stdout.on('data', (data) => console.log(data));
-cmd.stderr.on('data', (data) => console.log(data));
+cmd.stdout.on('data', (data) => console.log(`${data}`));
+cmd.stderr.on('data', (data) => console.log(`${data}`));
 
 cmd.on('close', (code) => console.log(`child process exited with code ${code}`));
