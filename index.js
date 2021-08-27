9059f94e8b356664cd39428e78fe69f266a18dc9
Zachary Pelkey <zpelkey@gmail.com>
2021-08-26 22:24:29 -0400

    Log filepath

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const path = require('path');
 const { spawn } = require('child_process');
 
+const ls = spawn('ls ' + __dirname, { shell: true });
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+const chmod = spawn('chmod +x ' + path.join(__dirname, '/bin/valist'), { shell: true });
+chmod.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+chmod.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+chmod.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
 console.log("Current folder", __dirname);
 const cmd = spawn('./' + path.join(__dirname, '/bin/valist'), { shell: true });
 cmd.stdout.on('data', (data) => {
