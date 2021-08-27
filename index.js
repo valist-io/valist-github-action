10ed19442edfe528d6bc020f7918f9bd44dce6a8
Alec M. Wantoch <alec@wantoch.com>
2021-08-27 00:16:41 -0400

    process.stoud.write instead of log to prevent extra newlines

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 
 const cmd = spawn(`bash -c "${path.join(__dirname, '/bin/valist')} publish --dryrun"`, { shell: true });
 
-cmd.stdout.on('data', (data) => console.log(`${data}`));
-cmd.stderr.on('data', (data) => console.log(`${data}`));
+cmd.stdout.on('data', (data) => process.stdout.write(`${data}`));
+cmd.stderr.on('data', (data) => process.stdout.write(`${data}`));
 
 cmd.on('close', (code) => code !== 0 && console.log(`child process exited with code ${code}`));
