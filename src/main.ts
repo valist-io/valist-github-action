7a66d08cfa83b659fb6e4ae34e77a7477c354230
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-03-23 18:04:00 -0700

    build working

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import * as core from '@actions/core';
+import * as glob from '@actions/glob';
+import { ethers } from 'ethers';
+import { createClient, ReleaseMeta } from '../../valist-meta/valist-js/packages/valist-sdk/dist';
 
-try {
-	const team = core.getInput('team', { required: true });
-	const project = core.getInput('project', { required: true });
-	const release = core.getInput('release', { required: true });
-	const files = core.getMultilineInput('files', { required: true });  	
-} catch (err) {
-	core.setFailed(`Action failed with error ${err}`);
+async function run(): Promise<void> {
+	try {
+		const teamName = core.getInput('team', { required: true });
+		const projectName = core.getInput('project', { required: true });
+		const releaseName = core.getInput('release', { required: true });
+		const files = core.getInput('files', { required: true });
+		const privateKey = core.getInput('private-key', { required: true });
+
+		const provider = new ethers.providers.JsonRpcProvider('https://rpc.valist.io');
+		//const wallet = new ethers.Wallet(privateKey); TODO
+		const valist = createClient(provider);
+
+		const followSymbolicLinks = core.getBooleanInput('follow-symbolic-links');
+		const globber = await glob.create(files, { followSymbolicLinks });
+		const metaURI = await valist.writeFolder(globber.globGenerator());
+		
+		const release = new ReleaseMeta();
+		release.name = releaseName;
+		release.external_url = metaURI;
+		release.image = core.getInput('image');
+		release.description = core.getInput('description');
+
+		const tx = await valist.createRelease(teamName, projectName, releaseName, release);
+		tx.wait();
+	} catch (err: any) {
+		core.setFailed(err.message);
+	}
 }
+
+run();
