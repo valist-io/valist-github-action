eaab10d98a779157656fd9038bd322f0503e2ea2
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-03-25 14:40:52 -0700

    add globFiles function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as glob from '@actions/glob';
+import * as fs from 'fs';
 import { ethers } from 'ethers';
 import { createClient, ReleaseMeta } from '../../valist-meta/valist-js/packages/valist-sdk/dist';
 
@@ -8,7 +9,6 @@ async function run(): Promise<void> {
 		const accountName = core.getInput('account', { required: true });
 		const projectName = core.getInput('project', { required: true });
 		const releaseName = core.getInput('release', { required: true });
-		const files = core.getInput('files', { required: true });
 		const privateKey = core.getInput('private-key', { required: true });
 
 		const provider = new ethers.providers.JsonRpcProvider('https://rpc.valist.io');
@@ -16,10 +16,10 @@ async function run(): Promise<void> {
 		const valist = createClient(provider, signer);
 
 		const followSymbolicLinks = core.getBooleanInput('follow-symbolic-links');
-		const globber = await glob.create(files, { followSymbolicLinks });
+		const files = core.getInput('files', { required: true });
 
 		core.info('uploading files...');
-		const metaURI = await valist.writeFolder(globber.globGenerator());
+		const metaURI = await valist.writeFolder(globFiles(files, followSymbolicLinks));
 		core.info(`release URI ${metaURI}`);
 		
 		const release = new ReleaseMeta();
@@ -34,6 +34,24 @@ async function run(): Promise<void> {
 		tx.wait();
 	} catch (err: any) {
 		core.setFailed(err.message);
+	}
+}
+
+async function * globFiles(files: string, followSymbolicLinks: boolean) {
+	const globber = await glob.create(files, { followSymbolicLinks });
+
+	for await (const path of globber.globGenerator()) {
+	    const stat = await fs.promises.stat(path);
+	    const content = stat.isFile() 
+	    	? fs.createReadStream(path) 
+	    	: undefined;
+
+	    yield {
+	      path: path,
+	      content: content,
+	      mode: stat.mode,
+	      mtime: stat.mtime,
+	    }
 	}
 }
 
